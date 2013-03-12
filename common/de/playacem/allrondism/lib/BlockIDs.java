6a0f1440f89d13cc38bac34275c5b0cc6eefeef6
Playacem <playacem@gmail.com>
2013-03-12 22:00:25 +0100

    ID files for my mod.

---
+++ common/de/playacem/allrondism/lib/BlockIDs.java
@@ -0,0 +1,10 @@
+package de.playacem.allrondism.lib;
+
+public class BlockIDs {
+	//TODO configuration file reading
+	public static final int STORAGE_DIRT_DEFAULT = 500;
+	public static final int STORAGE_ALLRONDIUM_DEFAULT = 502;
+	public static final int STORAGE_COBBLE_DEFAULT = 503;
+
+	public static final int ORE_ALLRONDIUM_DEFAULT = 501;
+}
---
+++ common/de/playacem/allrondism/lib/ItemIDs.java
@@ -0,0 +1,6 @@
+package de.playacem.allrondism.lib;
+
+public class ItemIDs {
+	//TODO reading from a config file
+	public static final int ALLRONDIUM_GEM_DEFAULT = 5000; 
+}
