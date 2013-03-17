2f662cec168699675d246f62ca71ce2aca4d2a61
Playacem <playacem@gmail.com>
2013-03-17 10:49:10 +0100

    cleaned up imports

--- common/de/playacem/allrondism/configuration/ConfigurationHandler.java
+++ common/de/playacem/allrondism/configuration/ConfigurationHandler.java
@@ -1,14 +1,10 @@
 package de.playacem.allrondism.configuration;
 
-import static net.minecraftforge.common.Configuration.CATEGORY_GENERAL;
-
 import java.io.File;
 import java.util.logging.Level;
 
 import net.minecraftforge.common.Configuration;
-import net.minecraftforge.common.Property;
 
-import de.playacem.allrondism.AllrondismMod;
 import de.playacem.allrondism.lib.BlockIDs;
 import de.playacem.allrondism.lib.ItemIDs;
 import de.playacem.allrondism.lib.Reference;
@@ -42,7 +38,7 @@ public class ConfigurationHandler {
 			BlockIDs.STORAGE_COBBLE = configuration.getBlock(Strings.STORAGE_COBBLE_NAME, BlockIDs.STORAGE_COBBLE_DEFAULT).getInt(BlockIDs.STORAGE_COBBLE_DEFAULT);
 			BlockIDs.STORAGE_ALLRONDIUM = configuration.getBlock(Strings.STORAGE_ALLRONDIUM_NAME, BlockIDs.STORAGE_ALLRONDIUM_DEFAULT).getInt(BlockIDs.STORAGE_ALLRONDIUM_DEFAULT);
 			
-			/* Item configs*/
+			/* Item configs */
 			ItemIDs.ALLRONDIUM_GEM = configuration.getItem(Strings.ALLRONDIUM_GEM, ItemIDs.ALLRONDIUM_GEM_DEFAULT).getInt(ItemIDs.ALLRONDIUM_GEM_DEFAULT);
 			
 		}
