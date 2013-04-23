ccfbfa20c91a62b574f06ef2508ebc16a6c63b7f
Playacem <playacem@gmail.com>
2013-04-23 17:52:36 +0200

    Enabled a name for the Custom Tab

--- common/de/playacem/allrondism/AllrondismMod.java
+++ common/de/playacem/allrondism/AllrondismMod.java
@@ -7,6 +7,7 @@ package de.playacem.allrondism;
 import java.io.File;
 
 import net.minecraft.creativetab.CreativeTabs;
+
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
@@ -17,6 +18,8 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+
 import de.playacem.allrondism.block.ModBlocks;
 import de.playacem.allrondism.configuration.ConfigurationHandler;
 import de.playacem.allrondism.core.helper.LogHelper;
@@ -24,9 +27,7 @@ import de.playacem.allrondism.core.proxy.CommonProxy;
 import de.playacem.allrondism.creativetab.CreativeTabAM;
 import de.playacem.allrondism.item.ModItems;
 import de.playacem.allrondism.lib.Reference;
-//Vanilla Minecraft related imports
-//personal imports
-//General modloading imports
+
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
@@ -58,9 +59,10 @@ public class AllrondismMod {
         LogHelper.init();
 
         // Initialize the configuration
-        ConfigurationHandler.init(new File(event.getModConfigurationDirectory()
-                .getAbsolutePath() + "\\" + Reference.MOD_ID + ".cfg"));
+        ConfigurationHandler.init(new File(event.getModConfigurationDirectory().getAbsolutePath() + File.separator + Reference.MOD_ID + ".cfg"));
 
+        //adding a name for the Custom Tab
+        LanguageRegistry.instance().addStringLocalization("itemGroup." + Reference.MOD_ID, "en_US", Reference.MOD_NAME);
         // Initialize mod items
         ModItems.init();
 
