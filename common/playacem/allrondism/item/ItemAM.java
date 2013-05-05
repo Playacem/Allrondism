cd5416102317340b773b1a984ed171281d78883f
Playacem <playacem@gmail.com>
2013-05-05 21:23:12 +0200

    code cleanup

--- common/playacem/allrondism/item/ItemAM.java
+++ common/playacem/allrondism/item/ItemAM.java
@@ -18,9 +18,6 @@ public class ItemAM extends Item {
     @Override
     @SideOnly(Side.CLIENT)
     public void registerIcons(IconRegister iconRegister) {
-        itemIcon = iconRegister.registerIcon(Reference.MOD_ID.toLowerCase()
-                + ":"
-                + this.getUnlocalizedName().substring(
-                        this.getUnlocalizedName().indexOf(".") + 1));
+        itemIcon = iconRegister.registerIcon(Reference.MOD_ID.toLowerCase() + ":" + this.getUnlocalizedName().substring( this.getUnlocalizedName().indexOf(".") + 1));
     }
 }
