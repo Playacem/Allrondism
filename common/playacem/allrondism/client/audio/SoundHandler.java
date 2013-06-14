cad998f5fa56c8b413350ba027cf0211400dea56
Playacem <playacem@gmail.com>
2013-06-14 11:19:22 +0200

    updated SoundHandler, still WIP

--- common/playacem/allrondism/client/audio/SoundHandler.java
+++ common/playacem/allrondism/client/audio/SoundHandler.java
@@ -1,11 +1,8 @@
 package playacem.allrondism.client.audio;
 
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.World;
 import net.minecraftforge.client.event.sound.PlaySoundEvent;
 import net.minecraftforge.event.ForgeSubscribe;
 import playacem.allrondism.core.util.LogHelper;
-import playacem.allrondism.lib.BlockIDs;
 
 /**
  * Allrondism
@@ -20,25 +17,14 @@ public class SoundHandler {
 
     @ForgeSubscribe
     public void onSoundPlay(PlaySoundEvent event) {
-        return;
-        /*
-        //TODO Get the World using DimensionManager.
-        //need a way to get the current dimension the client is in.
-        World world = Minecraft.getMinecraft().theWorld;
-        //World world = DimensionManager.getWorld(dimID);
-        int x = (int) event.x;
-        int y = (int) event.y;
-        int z = (int) event.z;
+
+        LogHelper.info("Event: " + event.name);
         if (event.name.contains("step.stone")) {
-            if(world.getBlockId(x, y, z)== BlockIDs.STORAGE_BLOCKS ){
-                if(world.getBlockMetadata(x, y, z) == 0) {
-                    event.manager.pauseAllSounds();
-                    event.manager.playSound("step.gravel", event.x, event.y, event.z, 0.15F, 1.0F);
-                    event.manager.resumeAllSounds();
-                }
-            }
+            LogHelper.info("Stone Event!");
+            //event.result = event.manager.soundPoolSounds.getRandomSoundFromSoundPool("step.gravel");
+            // TODO add check for specific block 
         }
-        */
+        
     }
 
     
