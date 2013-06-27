768900c8bd1231a43c535601001ee3921507865c
Playacem <playacem@gmail.com>
2013-06-27 10:07:10 +0200

    added more log functions, one for each Level

--- common/playacem/allrondism/core/util/LogHelper.java
+++ common/playacem/allrondism/core/util/LogHelper.java
@@ -24,36 +24,57 @@ public class LogHelper {
         amLogger.setParent(FMLLog.getLogger());
     }
 
-    /**
-     * Logs a message.
-     * 
-     * @see info
-     * @see alert
-     * @param logLevel
-     * @param message
-     */
+
     public static void log(Level logLevel, String message) {
 
         amLogger.log(logLevel, message);
     }
 
-    /**
-     * Logs a message. Uses INFO level
-     * 
-     * @param message
-     */
+    
     public static void info(String message) {
 
         amLogger.log(Level.INFO, message);
     }
 
     /**
-     * Logs a message Uses WARNING level
-     * 
-     * @param message
+     * Uses WARNING level.
      */
     public static void alert(String message) {
 
         amLogger.log(Level.WARNING, message);
     }
+    
+    public static void severe(String message) {
+        
+        amLogger.log(Level.SEVERE, message);
+    }
+    
+    public static void config(String message) {
+        
+        amLogger.log(Level.CONFIG, message);
+    }
+    
+    public static void fine(String message) {
+        
+        amLogger.log(Level.FINE, message);
+    }
+    
+    public static void finer(String message) {
+        
+        amLogger.log(Level.FINER, message);
+    }
+    
+    public static void finest(String message) {
+        
+        amLogger.log(Level.FINEST, message);
+    }
+    
+    /**
+     * Use {@link playacem.allrondism.core.util.LogHelper#alert(String) alert} instead
+     */
+    public static void warning(String message) {
+        
+        amLogger.log(Level.WARNING, message);
+    }
+    
 }
