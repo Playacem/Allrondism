f5eb43dca87d44cc31bc9b8aed7c6532ceae93b0
Playacem <playacem@gmail.com>
2013-07-07 19:17:13 +0200

    renamed Sprites.java to Textures.java

---
+++ common/playacem/allrondism/lib/Textures.java
@@ -0,0 +1,31 @@
+package playacem.allrondism.lib;
+
+/**
+ * Allrondism
+ * 
+ * Textures
+ * 
+ * @author Playacem
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ * @credit pahimar
+ */
+public class Textures {
+
+    // Base file paths
+    public static final String MODEL_SHEET_LOCATION = "/mods/allrondism/textures/models";
+    public static final String ARMOR_SHEET_LOCATION = "/mods/allrondism/textures/armor/";
+    public static final String GUI_SHEET_LOCATION = "/mods/allrondism/textures/gui/";
+    public static final String EFFECTS_LOCATION = "/mods/allrondism/textures/effects/";
+
+    // Item/Block sprite sheets
+    public static final String VANILLA_ITEM_TEXTURE_SHEET = "/gui/items.png";
+    public static final String VANILLA_BLOCK_TEXTURE_SHEET = "/terrain.png";
+    
+    // Model textures
+    
+    // Armor sprite sheets
+    
+    // GUI textures
+    
+    // Effect textures
+}
