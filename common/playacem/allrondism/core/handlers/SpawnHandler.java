515e83f1d36c30db961c2b331acc285c9ac7fa74
Playacem <playacem@gmail.com>
2013-07-16 10:30:09 +0200

    finished the SpawnHandler, now nothing can Spawn on my Storage Blocks

--- common/playacem/allrondism/core/handlers/SpawnHandler.java
+++ common/playacem/allrondism/core/handlers/SpawnHandler.java
@@ -1,7 +1,9 @@
 package playacem.allrondism.core.handlers;
 
+import playacem.allrondism.lib.BlockIDs;
+import net.minecraftforge.event.Event.Result;
 import net.minecraftforge.event.ForgeSubscribe;
-import net.minecraftforge.event.entity.living.LivingSpawnEvent;
+import net.minecraftforge.event.entity.living.LivingSpawnEvent.CheckSpawn;
 
 /**
  * Allrondism
@@ -14,10 +16,17 @@ import net.minecraftforge.event.entity.living.LivingSpawnEvent;
  */
 public class SpawnHandler {
 
-
     @ForgeSubscribe
-    public void onEntitySpawn(LivingSpawnEvent event) {
-        // TODO disable Spawns for certain blocks
-    }
+    public void onEntitySpawn(CheckSpawn event) {
+        int x = (int)Math.floor((double)event.x);
+        int y = (int)Math.floor((double)event.y);
+        int z = (int)Math.floor((double)event.z);
+        y--;//block below the spawn place of the mob
 
+        if (event.world.getBlockId(x, y, z) == BlockIDs.STORAGE_BLOCKS) {
+            event.setResult(Result.DENY);
+        }else {
+            event.setResult(Result.DEFAULT);
+        }
+    }
 }
