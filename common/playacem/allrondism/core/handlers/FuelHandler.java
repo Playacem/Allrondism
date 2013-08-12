5f91fb1256f6655400f7dd52208b4c25b9738a6b
Playacem <playacem@gmail.com>
2013-08-12 11:44:34 +0200

    added bedrock as a fuel

--- common/playacem/allrondism/core/handlers/FuelHandler.java
+++ common/playacem/allrondism/core/handlers/FuelHandler.java
@@ -1,5 +1,6 @@
 package playacem.allrondism.core.handlers;
 
+import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import cpw.mods.fml.common.IFuelHandler;
 
@@ -9,6 +10,9 @@ public class FuelHandler implements IFuelHandler {
     @Override
     public int getBurnTime(ItemStack fuel) {
         // TODO Add fuel relevant items
+        if (fuel.itemID == Block.bedrock.blockID) {
+            return 200 * 9001;
+        }
         return 0;
     }
 
