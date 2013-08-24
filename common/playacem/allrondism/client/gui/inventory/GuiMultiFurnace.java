aeb9d1203b2d0508db4112e27b3c8b46408a2eb7
Playacem <playacem@gmail.com>
2013-08-24 20:12:58 +0200

    finished gui

--- common/playacem/allrondism/client/gui/inventory/GuiMultiFurnace.java
+++ common/playacem/allrondism/client/gui/inventory/GuiMultiFurnace.java
@@ -1,10 +1,15 @@
 package playacem.allrondism.client.gui.inventory;
 
+import org.lwjgl.opengl.GL11;
+
 import playacem.allrondism.inventory.ContainerMultiFurnace;
+import playacem.allrondism.lib.Strings;
+import playacem.allrondism.lib.Textures;
 import playacem.allrondism.tileentity.TileEntityMultiFurnaceCore;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.util.StatCollector;
 
 /**
  * Allrondism
@@ -16,20 +21,32 @@ import net.minecraft.inventory.Container;
  */
 public class GuiMultiFurnace extends GuiContainer {
 
-    private TileEntityMultiFurnaceCore tileEntity;
+    private TileEntityMultiFurnaceCore tileCore;
+    
     public GuiMultiFurnace(InventoryPlayer playerInventory, TileEntityMultiFurnaceCore tileEntity) {
         super(new ContainerMultiFurnace(playerInventory, tileEntity));
         
-        this.tileEntity = tileEntity;
+        this.tileCore = tileEntity;
     }
+    
+    @Override
+    protected void drawGuiContainerForegroundLayer(int par1, int par2) {
 
+        String containerName = tileCore.isInvNameLocalized() ? tileCore.getInvName() : "Multi-Furnace";
+        fontRenderer.drawString(containerName, xSize / 2 - fontRenderer.getStringWidth(containerName) / 2,  6, 4210752);
+        fontRenderer.drawString(StatCollector.translateToLocal(Strings.CONTAINER_INVENTORY), 8, ySize - 93, 4210752);
+    }
+    
     @Override
     protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
 
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        mc.renderEngine.bindTexture(Textures.GUI_MULTI_FURNACE);
+        int xStart = (width - xSize) / 2;
+        int yStart = (height - ySize) / 2;
+        this.drawTexturedModalRect(xStart, yStart, 0, 0, xSize, ySize);
+       
     }
 
-    @Override
-    protected void drawGuiContainerForegroundLayer(int par1, int par2) {
-        
-    }
+    
 }
