5e2f8fb9504772806d31ade41551614b67be0c36
Playacem <playacem@gmail.com>
2013-09-05 14:18:22 +0200

    the container, still not finished

--- common/playacem/allrondism/inventory/ContainerMultiFurnace.java
+++ common/playacem/allrondism/inventory/ContainerMultiFurnace.java
@@ -4,6 +4,7 @@ import playacem.allrondism.tileentity.TileEntityMultiFurnaceCore;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
 
 /**
  * Allrondism
@@ -14,16 +15,31 @@ import net.minecraft.inventory.Container;
  * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
  */
 public class ContainerMultiFurnace extends Container {
-    private TileEntityMultiFurnaceCore tileEntity;
+    private TileEntityMultiFurnaceCore tileCore;
+    private final int PLAYER_INVENTORY_ROWS = 3;
+    private final int PLAYER_INVENTORY_COLUMNS = 9;
     
     public ContainerMultiFurnace(InventoryPlayer playerInventory, TileEntityMultiFurnaceCore tileEntity) {
-        this.tileEntity = tileEntity;
+        this.tileCore = tileEntity;
+        
+        bindPlayerInventory(playerInventory);
     }
 
     @Override
     public boolean canInteractWith(EntityPlayer entityplayer) {
 
-        return tileEntity.isUseableByPlayer(entityplayer);
+        return tileCore.isUseableByPlayer(entityplayer);
     }
-
+    
+    private void bindPlayerInventory(InventoryPlayer playerInventory) {
+        // Inventory
+        for(int inventoryRowIndex = 0; inventoryRowIndex < PLAYER_INVENTORY_ROWS; ++inventoryRowIndex) {
+            for(int inventoryColumnIndex = 0; inventoryColumnIndex < PLAYER_INVENTORY_COLUMNS; ++inventoryColumnIndex) {
+                addSlotToContainer(new Slot(playerInventory, inventoryColumnIndex + inventoryRowIndex * 9 + 9, 8 + inventoryColumnIndex * 18 , 84 + inventoryRowIndex * 18));
+            }
+        }
+        // Action Bar
+        for(int actionBarSlotIndex = 0; actionBarSlotIndex < PLAYER_INVENTORY_COLUMNS; ++actionBarSlotIndex)
+            addSlotToContainer(new Slot(playerInventory, actionBarSlotIndex, 8 + actionBarSlotIndex * 18, 142));
+    }
 }
