1d3c03e35eb937a110f64c64526cdb95fc218898
Playacem <playacem@gmail.com>
2013-09-05 14:16:40 +0200

    added some graphical code

--- common/playacem/allrondism/client/gui/inventory/GuiMultiFurnace.java
+++ common/playacem/allrondism/client/gui/inventory/GuiMultiFurnace.java
@@ -8,7 +8,6 @@ import playacem.allrondism.lib.Textures;
 import playacem.allrondism.tileentity.TileEntityMultiFurnaceCore;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
-import net.minecraft.inventory.Container;
 import net.minecraft.util.StatCollector;
 
 /**
@@ -45,6 +44,17 @@ public class GuiMultiFurnace extends GuiContainer {
         int xStart = (width - xSize) / 2;
         int yStart = (height - ySize) / 2;
         this.drawTexturedModalRect(xStart, yStart, 0, 0, xSize, ySize);
+        int timeScaled;
+        int debug = -16;
+        
+        if(tileCore.isBurning()) {
+            timeScaled = tileCore.getBurnTimeRemainingScaled(12);
+            drawTexturedModalRect(xStart + 56 + debug, yStart + 36 + 12 - timeScaled, 176, 12 - timeScaled, 14, timeScaled + 2);
+            //^needs to be tested!
+        }
+        
+        timeScaled = tileCore.getCookProgressScaled(24);
+        drawTexturedModalRect(xStart + 79, yStart + 34, 176, 14, timeScaled + 1, 16);
        
     }
 
