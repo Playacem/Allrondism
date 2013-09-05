f9d4a87e5051068f2f308668645f0c54090352cf
Playacem <playacem@gmail.com>
2013-09-05 14:17:11 +0200

    added Bedrock as a fuel option, it is OVER 9000!

--- common/playacem/allrondism/core/handlers/FuelHandler.java
+++ common/playacem/allrondism/core/handlers/FuelHandler.java
@@ -11,7 +11,7 @@ public class FuelHandler implements IFuelHandler {
     public int getBurnTime(ItemStack fuel) {
         // TODO Add fuel relevant items
         if (fuel.itemID == Block.bedrock.blockID) {
-            return 200 * 9001;
+            return 200 * 9001; // It's over 9000!
         }
         return 0;
     }
