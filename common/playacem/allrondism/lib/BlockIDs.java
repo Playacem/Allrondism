612002aae381c6b1f228c0e757d73a5081baa7bc
Playacem <playacem@gmail.com>
2013-09-05 14:14:22 +0200

    split up the multifurnace in 2 blocks

--- common/playacem/allrondism/lib/BlockIDs.java
+++ common/playacem/allrondism/lib/BlockIDs.java
@@ -15,13 +15,15 @@ public class BlockIDs {
     public static int STORAGE_BLOCKS_DEFAULT = 501;
     public static int GLASS_SUN_BLOCKER_DEFAULT = 502;
     public static int PLANT_ROSE_DEFAULT = 503;
-    public static int MULTI_FURNACE_DEFAULT = 504;
+    public static int MULTI_FURNACE_CORE_DEFAULT = 504;
+    public static int MULTI_FURNACE_EXTENSION_DEFAULT = 505;
 
     /* Current block ids */
     public static int STORAGE_BLOCKS;
     public static int ORE_ALLRONDIUM;
     public static int GLASS_SUN_BLOCKER;
     public static int PLANT_ROSE;
-    public static int MULTI_FURNACE;
+    public static int MULTI_FURNACE_CORE;
+    public static int MULTI_FURNACE_EXTENSION;
 
 }
