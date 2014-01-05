211da5cd9adbcf1dcd61eca639232e6f9d5749cd
Playacem <playacem@gmail.com>
2014-01-05 19:10:57 +0100

    made LogHelper.java more universal useable

--- common/playacem/allrondism/core/util/LogHelper.java
+++ common/playacem/allrondism/core/util/LogHelper.java
@@ -3,7 +3,7 @@ package playacem.allrondism.core.util;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import playacem.allrondism.configuration.ConfigurationSettings;
+import playacem.allrondism.configuration.Settings;
 import playacem.allrondism.lib.Reference;
 import cpw.mods.fml.common.FMLLog;
 
@@ -25,20 +25,20 @@ public class LogHelper {
         amLogger.setParent(FMLLog.getLogger());
     }
 
-    public static void log(Level logLevel, String message) {
+    public static void log(Level logLevel, Object object) {
 
-        amLogger.log(logLevel, message);
+        amLogger.log(logLevel, object.toString());
     }
 
-    public static void info(String message) {
+    public static void info(Object object) {
 
-        log(Level.INFO, message);
+        log(Level.INFO, object);
     }
 
     /** Uses WARNING level. */
-    public static void alert(String message) {
+    public static void alert(Object object) {
 
-        log(Level.WARNING, message);
+        log(Level.WARNING, object);
     }
 
     /**
@@ -46,55 +46,55 @@ public class LogHelper {
      * {@link playacem.allrondism.core.util.LogHelper#debug(String, boolean)
      * this}
      */
-    public static void debug(String message) {
+    public static void debug(Object object) {
 
-        debug(message, false);
+        debug(object, false);
     }
 
     /** if true: WARNING, if false: INFO */
-    public static void debug(String message, boolean isWarningLevel) {
+    public static void debug(Object object, boolean isWarningLevel) {
 
-        if (ConfigurationSettings.DEBUG_MODE) {
+        if (Settings.DEBUG_MODE) {
             if (isWarningLevel) {
-                log(Level.WARNING, "[DEBUG] " + message);
+                log(Level.WARNING, "[DEBUG] " + object.toString());
             } else {
-                log(Level.INFO, "[DEBUG] " + message);
+                log(Level.INFO, "[DEBUG] " + object.toString());
             }
         }
     }
 
-    public static void severe(String message) {
+    public static void severe(Object object) {
 
-        log(Level.SEVERE, message);
+        log(Level.SEVERE, object);
     }
 
-    public static void config(String message) {
+    public static void config(Object object) {
 
-        log(Level.CONFIG, message);
+        log(Level.CONFIG, object);
     }
 
-    public static void fine(String message) {
+    public static void fine(Object object) {
 
-        log(Level.FINE, message);
+        log(Level.FINE, object);
     }
 
-    public static void finer(String message) {
+    public static void finer(Object object) {
 
-        log(Level.FINER, message);
+        log(Level.FINER, object);
     }
 
-    public static void finest(String message) {
+    public static void finest(Object object) {
 
-        log(Level.FINEST, message);
+        log(Level.FINEST, object);
     }
 
     /**
      * Use {@link playacem.allrondism.core.util.LogHelper#alert(String) alert}
      * instead
      */
-    public static void warning(String message) {
+    public static void warning(Object object) {
 
-        log(Level.WARNING, message);
+        log(Level.WARNING, object);
     }
 
 }
